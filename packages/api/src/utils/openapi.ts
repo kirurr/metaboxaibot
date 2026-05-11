1ce967f75db8a7c53801b126885aefb68bb6a85b
kirurr <timon09df@gmail.com>
2026-05-11 13:57:33 +0300

    feat(openapi): add utils
    AIBOX-3

---
+++ packages/api/src/utils/openapi.ts
@@ -0,0 +1,92 @@
+import { type RouteOptions } from "fastify";
+
+export const unauthorizedResponse = {
+  description: "Unauthorized",
+  type: "object",
+  properties: {
+    error: { type: "string" },
+  },
+};
+
+export const userNotFoundResponse = {
+  description: "User not found",
+  type: "object",
+  properties: {
+    error: { type: "string" },
+  },
+};
+
+export const userIsBlockedResponse = {
+  description: "User is blocked",
+  type: "object",
+  properties: {
+    error: { type: "string" },
+  },
+};
+
+export const conflictResponse = {
+  description: "Conflict",
+  type: "object",
+  properties: {
+    error: { type: "string" },
+  },
+};
+
+export const badRequestResponse = {
+  description: "Bad request",
+  type: "object",
+  properties: {
+    error: { type: "string" },
+    code: { type: "string" },
+  },
+};
+
+export const rateLimitResponse = {
+  description: "Rate limit exceeded",
+  type: "object",
+  properties: {
+    code: { type: "string" },
+    error: { type: "string" },
+    retryAfterSec: { type: "number" },
+    attemptsLeft: { type: "number" },
+  },
+};
+
+export const metaboxApiErrorResponse = {
+  description: "Metabox API error",
+  type: "object",
+  properties: {
+    error: { type: "string" },
+    code: { type: "string" },
+  },
+};
+
+export const gatewayErrorResponse = {
+  description: "Gateway error",
+  type: "object",
+  properties: {
+    error: { type: "string" },
+  },
+};
+
+export function constructOpenAPIonRouteHook(routeOptions: RouteOptions, tags: string[]) {
+  routeOptions.schema ??= {};
+  routeOptions.schema.response ??= {};
+
+  // @ts-expect-error response is unknown
+  if (!routeOptions.schema.response[401]) {
+    // @ts-expect-error response is unknown
+    routeOptions.schema.response[401] = unauthorizedResponse;
+    // @ts-expect-error response is unknown
+    routeOptions.schema.response[404] = userNotFoundResponse;
+    // @ts-expect-error response is unknown
+    routeOptions.schema.response[403] = userIsBlockedResponse;
+  }
+
+  routeOptions.schema.tags ??= tags;
+  routeOptions.schema.security ??= [
+    {
+      telegramAuth: [],
+    },
+  ];
+}
